c13616e4d0aaafec578545fa844a42f06f3e6d53
Sivan Patel <sivan.patel@googlemail.com>
2018-02-12 16:59:02 +0000

    reintroduced test deleted by mistake

--- spec/bitmap_spec.rb
+++ spec/bitmap_spec.rb
@@ -7,4 +7,8 @@ describe Bitmap do
   it 'prints the bitmap with the given intializer values' do
     expect(bitmap.print).to eq ("OO\nOO")
   end
+
+  it 'raises an error if the bitmap coordinates are too big' do
+    expect{ described_class.new(251, 200) }.to raise_error "Coordinates out of bounds, must be between 1 and 250"
+  end
 end
